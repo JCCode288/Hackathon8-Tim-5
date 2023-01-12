ed9eb2928352211cb18021249dea82faba1edbf3
mfahriferdiansyah <xkadzuvic@gmail.com>
2023-01-12 21:14:02 +0700

    +Index.js, +Submit.js

--- Component/submit.js
+++ Component/submit.js
@@ -1,42 +1,23 @@
 function submit() {
   let form = document.forms.personForm;
-
-  let vaksinId = [
-    `vacc1`,
-    `vacc2`,
-    `vacc3`,
-    `vacc4`,
-    `vacc5`,
-    `vacc6`,
-    `vacc7`,
-    `vacc8`,
-  ];
-  let vaksinNama = [
-    `Polio`,
-    `Dengue`,
-    `Rotaro Virus`,
-    `Influenza`,
-    `Tifoid`,
-    `HPV`,
-    `Hepatitis A`,
-    `Hepatitis B`,
-  ];
+  let vaksinId = JSON.parse(localStorage.getItem("vaksinId"));
+  let vaksinNama = JSON.parse(localStorage.getItem("vaksinNama"));
 
   form.addEventListener("submit", (event) => {
     let formToSend = {};
     formToSend["name"] = form["name"].value;
     formToSend["tanggalLahir"] = form["umur"].value;
-
+    formToSend["vaccined"] = {};
     for (let i = 0; i < vaksinId.length; i++) {
       if (document.getElementById(vaksinId[i]).checked) {
-        formToSend[vaksinNama[i]] = true;
+        formToSend["vaccined"][vaksinNama[i]] = true;
       } else {
-        formToSend[vaksinNama[i]] = false;
+        formToSend["vaccined"][vaksinNama[i]] = false;
       }
     }
 
-    let e = document.getElementById(`city`)
-    formToSend['tempatTinggal'] = e.options[e.selectedIndex].text;
+    let kota = document.getElementById(`city`);
+    formToSend["tempatTinggal"] = kota.options[kota.selectedIndex].text;
     localStorage.setItem(`person`, JSON.stringify(formToSend));
     console.log(document.forms.personForm);
   });
--- index.js
+++ index.js
@@ -2,137 +2,163 @@ import deleteLog from "./Component/deleteLog.js";
 import deletingForm from "./Component/deletingForm.js";
 import submit from "./Component/submit.js";
 
-let databaseSurabaya = [
-  [1, "Asemrowo", "dr. Diah Miryati", "Jln. Asem Raya No. 8", "(031) 5456122"],
-  [
-    2,
-    "Balas Klumprik",
-    "dr. Yessy Pebriaty Sukatendel. MM",
-    "Jln. Raya Balas Klumprik",
-    "(031)7665219",
-  ],
-  [
-    3,
-    "Balongsari",
-    "dr. Dewi Ayuning Asih",
-    "Jln. Balongsari Tama No. 2 Surabaya",
-    "(031) 7417104",
-  ],
-  [
-    4,
-    "Bangkingan",
-    "dr. Winartuti Santoso, M.Kes",
-    "Jln. Banyu Urip Kidul 6/8",
-    "(031) 5685424",
-  ],
-  [
-    5,
-    "Banyu Urip",
-    "dr. Aloysius Tri Joehanto, MH",
-    "Jln. Raya Benowo No. 48",
-    "(031) 7405936",
-  ],
-  [
-    6,
-    "Benowo",
-    "dr. Aloysius Tri Joehanto",
-    "MH	Jln. Raya Benowo No. 48",
-    "(031) 7405936",
-  ],
-  [
-    7,
-    "Bulak Banteng",
-    "drg. Elfi Asriningdiah Poedji Utami",
-    "Jln. Dukuh Bulak Banteng Perintis Utama Lebar No.35",
-    "(031) 3765209",
-  ],
-  [
-    8,
-    "Dr. Soetomo",
-    "dr. Murtiningrum",
-    "Jln. Pandegiling No.223-A",
-    "081334932059",
-  ],
-  [
-    9,
-    "Dukuh Kupang",
-    "dr. Khusnul Khowatin",
-    "Jln. Dukuh Kupang 25/48",
-    "(031) 5677615",
-  ],
-  [
-    10,
-    "Dupak",
-    "dr. RR. Endang Dwihastutiningsih",
-    "Jln. Dupak Bangunrejo Gg Poliklinik No.6 Surabaya",
-    "(031) 3531009",
-  ],
-  [
-    11,
-    "Gading",
-    "dr. Dwiastuti Setyorini",
-    "Jln. Kapas Lor 1 no 1 Surabaya",
-    "(031) 3767736",
-  ],
-  [
-    12,
-    "Gayungan",
-    "drg. Harijanti Judaningsih",
-    "Jln. Gayungsari Barat 124",
-    "(031) 8290043",
-  ],
-  [
-    13,
-    "Gundih",
-    "dr. Tita Pusparini, M. Kes",
-    "Jln. Margodadi 36 - 38 Surabaya",
-    "(031) 5476275",
-  ],
-  [14, "Gunung Anyar"],
-  [
-    15,
-    "Jagir",
-    "dr. Dessy Jumiaty Setia",
-    "Jln. Bendul Merisi No 1",
-    "(031) 8416926",
-  ],
-  [
-    16,
-    "Jemursari",
-    "drg. Siti Januarsih",
-    "Jln. Jemursari Selatan IV/5",
-    "(031)8414743",
-  ],
-  [
-    17,
-    "Jeruk",
-    "drg. Harjanti",
-    "Jln. Raya Menganti Jeruk 277A",
-    "(031) 7521680",
-  ],
-  [
-    18,
-    "Kalijudan",
-    "dr. Ageng Rusbaya",
-    "Jln. Kalijudan No. 123",
-    "(031) 3824566",
-  ],
-  [
-    19,
-    "Kalirungkut",
-    "dr. Bernadetta Martini",
-    "Jln. Rungkut Puskesmas No. 1",
-    "(031) 87006689",
-  ],
-  [
-    20,
-    "Kebonsari",
-    "dr. Reyner Meilaksana S.M.H.Kes",
-    "Jln. Kebonsari Manunggal No. 30 - 31",
-    "(031) 8294420",
-  ],
-];
 function storeDb() {
+  let databaseSurabaya = [
+    [
+      1,
+      "Asemrowo",
+      "dr. Diah Miryati",
+      "Jln. Asem Raya No. 8",
+      "(031) 5456122",
+    ],
+    [
+      2,
+      "Balas Klumprik",
+      "dr. Yessy Pebriaty Sukatendel. MM",
+      "Jln. Raya Balas Klumprik",
+      "(031)7665219",
+    ],
+    [
+      3,
+      "Balongsari",
+      "dr. Dewi Ayuning Asih",
+      "Jln. Balongsari Tama No. 2 Surabaya",
+      "(031) 7417104",
+    ],
+    [
+      4,
+      "Bangkingan",
+      "dr. Winartuti Santoso, M.Kes",
+      "Jln. Banyu Urip Kidul 6/8",
+      "(031) 5685424",
+    ],
+    [
+      5,
+      "Banyu Urip",
+      "dr. Aloysius Tri Joehanto, MH",
+      "Jln. Raya Benowo No. 48",
+      "(031) 7405936",
+    ],
+    [
+      6,
+      "Benowo",
+      "dr. Aloysius Tri Joehanto",
+      "MH	Jln. Raya Benowo No. 48",
+      "(031) 7405936",
+    ],
+    [
+      7,
+      "Bulak Banteng",
+      "drg. Elfi Asriningdiah Poedji Utami",
+      "Jln. Dukuh Bulak Banteng Perintis Utama Lebar No.35",
+      "(031) 3765209",
+    ],
+    [
+      8,
+      "Dr. Soetomo",
+      "dr. Murtiningrum",
+      "Jln. Pandegiling No.223-A",
+      "081334932059",
+    ],
+    [
+      9,
+      "Dukuh Kupang",
+      "dr. Khusnul Khowatin",
+      "Jln. Dukuh Kupang 25/48",
+      "(031) 5677615",
+    ],
+    [
+      10,
+      "Dupak",
+      "dr. RR. Endang Dwihastutiningsih",
+      "Jln. Dupak Bangunrejo Gg Poliklinik No.6 Surabaya",
+      "(031) 3531009",
+    ],
+    [
+      11,
+      "Gading",
+      "dr. Dwiastuti Setyorini",
+      "Jln. Kapas Lor 1 no 1 Surabaya",
+      "(031) 3767736",
+    ],
+    [
+      12,
+      "Gayungan",
+      "drg. Harijanti Judaningsih",
+      "Jln. Gayungsari Barat 124",
+      "(031) 8290043",
+    ],
+    [
+      13,
+      "Gundih",
+      "dr. Tita Pusparini, M. Kes",
+      "Jln. Margodadi 36 - 38 Surabaya",
+      "(031) 5476275",
+    ],
+    [14, "Gunung Anyar"],
+    [
+      15,
+      "Jagir",
+      "dr. Dessy Jumiaty Setia",
+      "Jln. Bendul Merisi No 1",
+      "(031) 8416926",
+    ],
+    [
+      16,
+      "Jemursari",
+      "drg. Siti Januarsih",
+      "Jln. Jemursari Selatan IV/5",
+      "(031)8414743",
+    ],
+    [
+      17,
+      "Jeruk",
+      "drg. Harjanti",
+      "Jln. Raya Menganti Jeruk 277A",
+      "(031) 7521680",
+    ],
+    [
+      18,
+      "Kalijudan",
+      "dr. Ageng Rusbaya",
+      "Jln. Kalijudan No. 123",
+      "(031) 3824566",
+    ],
+    [
+      19,
+      "Kalirungkut",
+      "dr. Bernadetta Martini",
+      "Jln. Rungkut Puskesmas No. 1",
+      "(031) 87006689",
+    ],
+    [
+      20,
+      "Kebonsari",
+      "dr. Reyner Meilaksana S.M.H.Kes",
+      "Jln. Kebonsari Manunggal No. 30 - 31",
+      "(031) 8294420",
+    ],
+  ];
+  let vaksinId = [
+    `vacc1`,
+    `vacc2`,
+    `vacc3`,
+    `vacc4`,
+    `vacc5`,
+    `vacc6`,
+    `vacc7`,
+    `vacc8`,
+  ];
+  let vaksinNama = [
+    `Polio`,
+    `Dengue`,
+    `Rotaro Virus`,
+    `Influenza`,
+    `Tifoid`,
+    `HPV`,
+    `Hepatitis A`,
+    `Hepatitis B`,
+  ];
   let objDb = {};
   for (const [noUrut, ...sisaArr] of databaseSurabaya) {
     if (sisaArr.length > 2 && sisaArr.length > 3) {
@@ -145,6 +171,8 @@ function storeDb() {
       objDb[noUrut]["noTelp"] = noTelp;
     }
   }
+  localStorage.setItem("vaksinId", JSON.stringify(vaksinId));
+  localStorage.setItem("vaksinNama", JSON.stringify(vaksinNama));
   localStorage.setItem("surabayaArr", JSON.stringify(objDb));
 }
 storeDb();
@@ -160,3 +188,10 @@ document.getElementById("clear").addEventListener("click", () => {
 // console.log(rambo);
 
 // export { form, deletingForm, submit, deleteLog };
+let lokasiBtn1 = document.getElementById("lokasiBtn1");
+let vaccined = JSON.parse(localStorage.getItem(`person`));
+// lokasiBtn1.addEventListener("click", () => {
+//   for (const keys in vaccined) {
+//   }
+// });
+console.log(lokasiBtn1);
